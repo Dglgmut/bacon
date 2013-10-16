ba967735364bd1e5cbc769d76339ad648cd0f724
Dglgmut <florez.renan@gmail.com>
2013-10-15 23:44:58 -0300

    adapting asana API

--- lib/bacon/addons/asana.rb
+++ lib/bacon/addons/asana.rb
@@ -5,12 +5,16 @@ require "net/https"
 module Bacon
   module Addons
     class Asana
-      def initialize(opts = {})
-        find_or_create_by_api_key_and_workspace_id(opts[:api_key], opts[:workspace_id])
+      attr_reader :api_key
+
+      def initialize(key)
+        find_or_create_by_api_key_and_workspace_id(key)
       end
 
+protected
+
       #the standard for recording a asana object is: asana_*keyName*
-      def find_or_create_by_api_key_and_workspace_id(api_key, workspace_id)
+      def find_or_create_by_api_key_and_workspace_id(api_key)
         stm = Bacon::DB.prepare "SELECT `object` FROM Bacon 
                                  WHERE`object` LIKE 'asana_%'"
 
@@ -18,15 +22,12 @@ module Bacon
         if rs.any?
           rs_hash = {}
           Array[*rs].each do |o|
-            puts o
             o1, o2 = o.to_s.split(':')
             rs_hash[o1] = o2
           end
           @api_key = rs_hash[:api_key]
-          @workspace_id = rs_hash[:workspace_id]
         else
           Bacon::DB.execute "INSERT INTO Bacon (object) VALUES('asana_api_key:#{api_key}')"
-          Bacon::DB.execute "INSERT INTO Bacon (object) VALUES('asana_workspace_id:#{workspace_id}')"
         end
       end
     end
--- spec/bacon/addons/asana_spec.rb
+++ spec/bacon/addons/asana_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Bacon::Addons::Asana do
   it "should find or create api_key and workspace_id at initialization" do
-    a = Bacon::Addons::Asana.new(api_key: 1, workspace_id: 1)
+    a = Bacon::Addons::Asana.new(1)
     stm = Bacon::DB.prepare "SELECT `object` FROM Bacon 
                              WHERE`object` LIKE 'asana_%'"
 
     rs = stm.execute
-    Array[*rs].length.should eq 2
+    Array[*rs].length.should eq 1
   end
 end
