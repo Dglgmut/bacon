bf130b35052901edd0c626924fff04a2089c0799
Dglgmut <florez.renan@gmail.com>
2013-10-16 00:21:41 -0300

    refactoring asana api

--- lib/bacon/addons/asana.rb
+++ lib/bacon/addons/asana.rb
@@ -8,27 +8,30 @@ module Bacon
       attr_reader :api_key
 
       def initialize(key)
-        find_or_create_by_api_key_and_workspace_id(key)
+        if find_by_api_key(key)
+          #get asana data
+        else
+          create_by_api_key(key)
+        end
       end
 
 protected
 
-      def find_or_create_by_api_key_and_workspace_id(api_key)
-        stm = Bacon::DB.prepare "SELECT `asana_key` FROM User 
-                                 WHERE`asana_key` = '#{api_key}'"
-
-        rs = stm.execute
-        if rs.any?
-          rs_hash = {}
-          Array[*rs].each do |o|
-            o1, o2 = o.to_s.split(':')
-            rs_hash[o1] = o2
-          end
-          @api_key = rs_hash[:api_key]
+      def find_by_api_key(api_key)
+        query = Bacon::DB.prepare("SELECT `asana_key` FROM User
+                                   WHERE `asana_key` = '#{api_key}'")
+        results = query.execute!
+        if @api_key = results[0][0]
+          self
         else
-          Bacon::DB.execute "INSERT INTO User (asana_key) VALUES('#{api_key}')"
+          false
         end
       end
+
+      def create_by_api_key(api_key)
+        Bacon::DB.execute "INSERT INTO User (asana_key) VALUES('#{api_key}')"
+      end
+
     end
   end
 end
--- spec/bacon/addons/asana_spec.rb
+++ spec/bacon/addons/asana_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 describe Bacon::Addons::Asana do
   it "should find or create api_key and workspace_id at initialization" do
-    a = Bacon::Addons::Asana.new(1)
-    stm = Bacon::DB.prepare "SELECT 1 FROM User 
-                             WHERE `asana_key` = 1"
+    a = Bacon::Addons::Asana.new('im_a_key')
+    stm = Bacon::DB.prepare "SELECT `asana_key` FROM User
+                             WHERE `asana_key` = 'im_a_key'"
 
     rs = stm.execute
-    Array[*rs].length.should eq 1
+    rs.next.first.should eq 'im_a_key'
   end
 end
